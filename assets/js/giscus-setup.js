774c3f64612cdb790e5d92cb5588d7617345894d
R11001 <ro110010398@gmail.com>
2026-05-06 09:14:41 +0000

    Deploying to gh-pages from @ R11001/R11001.github.io@0073327e3f20305df0951aa0e7baea9c186e67e1 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "R11001/R1001.github.io",
+    "data-repo": "R11001/R11001.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
